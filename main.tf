fbee1f362701db6cb94754f9e6e338c63a9385f4
vvramadm <60600569+vvramadm@users.noreply.github.com>
2025-06-01 23:00:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -140,8 +140,8 @@ resource "aws_route" "public" {
 
 resource "aws_route" "private" {
   route_table_id            = aws_route_table.private.id
-  destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.example.id
+  #destination_cidr_block    = "0.0.0.0/0"
+  #nat_gateway_id = aws_nat_gateway.example.id
 }
 
 resource "aws_route" "database" {
