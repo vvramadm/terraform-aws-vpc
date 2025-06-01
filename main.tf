56cd8c3a3ce0d651214076934a55ecf960f8c621
vvramadm <60600569+vvramadm@users.noreply.github.com>
2025-06-01 23:15:06 +0530

    Update main.tf
    terra

--- main.tf
+++ main.tf
@@ -75,26 +75,26 @@ resource "aws_subnet" "database" {
 }
 
 
-#resource "aws_eip" "nat" {
-#  domain   = "vpc"
-#}
+# resource "aws_eip" "nat" {
+#   domain   = "vpc"
+# }
 
-#resource "aws_nat_gateway" "example" {
- # allocation_id = aws_eip.nat.id
- # subnet_id     = aws_subnet.public[0].id
+# resource "aws_nat_gateway" "example" {
+#   allocation_id = aws_eip.nat.id
+#   subnet_id     = aws_subnet.public[0].id
 
- # tags = merge(
-  #  var.common_tags,
- #   var.nat_gateway_tags,
- #   {
- #     Name = local.resource_name
-  #  }
-#  )
+#   tags = merge(
+#     var.common_tags,
+#     var.nat_gateway_tags,
+#     {
+#       Name = local.resource_name
+#     }
+#   )
 
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-#  depends_on = [aws_internet_gateway.main]
-#}
+#   # To ensure proper ordering, it is recommended to add an explicit dependency
+#   # on the Internet Gateway for the VPC.
+#   depends_on = [aws_internet_gateway.main]
+# }
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
@@ -138,17 +138,17 @@ resource "aws_route" "public" {
   gateway_id = aws_internet_gateway.main.id
 }
 
-resource "aws_route" "private" {
-  route_table_id            = aws_route_table.private.id
-  #destination_cidr_block    = "0.0.0.0/0"
-  #nat_gateway_id = aws_nat_gateway.example.id
-}
+# resource "aws_route" "private" {
+#   route_table_id            = aws_route_table.private.id
+#   destination_cidr_block    = "0.0.0.0/0"
+#   nat_gateway_id = aws_nat_gateway.example.id
+# }
 
-resource "aws_route" "database" {
-  route_table_id            = aws_route_table.database.id
-  #destination_cidr_block    = "0.0.0.0/0"
- #nat_gateway_id = aws_nat_gateway.example.id
-}
+# resource "aws_route" "database" {
+#   route_table_id            = aws_route_table.database.id
+#   destination_cidr_block    = "0.0.0.0/0"
+#   nat_gateway_id = aws_nat_gateway.example.id
+# }
 
 resource "aws_route_table_association" "public" {
   count = length(var.public_subnet_cidrs)
