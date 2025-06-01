1a7170574c3682a9bf8c1ae7655287f9809a4a67
vvramadm <vvramadm@gmail.com>
2025-06-01 18:17:39 +0530

    terra

---
+++ data.tf
@@ -0,0 +1,15 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_route_table" "main" {
+  vpc_id = local.default_vpc_id
+  filter {
+    name = "association.main"
+    values = ["true"]
+  }
+}
---
+++ outputs.tf
@@ -0,0 +1,24 @@
+/* output "azs_info" {
+  value       = data.aws_availability_zones.available
+}
+ */
+
+#  output "subnets_info"{
+#     value = aws_subnet.public
+#  }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
