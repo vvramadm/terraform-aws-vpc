718c3276f57301416ae621ea762d06b83d9f0490
vvramadm <60600569+vvramadm@users.noreply.github.com>
2025-06-02 20:28:00 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals{
     resource_name = "${var.project_name}-${var.environment}"
-    az_names = slice(data.aws_availability_zones.available.names, 0, 2)
+    az_names = slice(data.aws_availability_zones.available.names, 0, 3)
     default_vpc_id = data.aws_vpc.default.id
     default_vpc_cidr = data.aws_vpc.default.cidr_block
 }
